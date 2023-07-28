a85b6a2e1dd46f83c02df0f4788f4d22c9ce6015
paulnyabaro <paulnyabaro1@gmail.com>
2023-07-28 06:32:02 +0300

    Working on book tests

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Book
+
+
+class BookTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title = 'A good title'
+            subtitle = 'An excellent subtitle'
+            author = 'Tom Christie'
+            isbn = '1234567890123'
+        )
+    
+    
+
